c48458df2cdebff58163a19b6e6e0cb34c36fc6f
ershuozi <liushuo2951@163.com>
2023-12-21 20:13:11 +0800

    init

---
+++ mybatis_quickstart/src/main/java/com/mapper/MonsterMapper.java
@@ -0,0 +1,11 @@
+package com.mapper;
+
+import com.entity.Monster;
+
+/**
+ * 该接口用于声明操作Monster表的方法，这些方法通过注解或者xml文件来实现
+ */
+public interface MonsterMapper {
+    //添加Monster
+    public void addMonster(Monster monster);
+}
